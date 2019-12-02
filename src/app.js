f7de69a0062e1abe35ce332ea64c82f3d09daec6
Enrico Massone <enrico.massone@deltatre.it>
2019-12-02 07:23:03 +0100

    completed a first example of a react application made without using the jsx stuff

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const Pet = () => {
+const Pet = ({name, animal, breed}) => {
   return React.createElement(
     "div",
     {},
     [
-      React.createElement("h1", {}, "Luna"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Havanese"),
+      React.createElement("h1", {}, name),
+      React.createElement("h2", {}, animal),
+      React.createElement("h2", {}, breed),
     ]
   );
 };
@@ -16,9 +16,28 @@ const App = () => {
     {},
     [
       React.createElement("h1", {}, "Adopt me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
+      React.createElement(
+        Pet,
+        {
+          name: "Luna",
+          animal: "Dog",
+          breed: "Havanese"
+        }),
+      React.createElement(
+        Pet,
+        {
+          name: "Doink",
+          animal: "Cat",
+          breed: "Mixed"
+        }
+        ),
+      React.createElement(
+        Pet,
+        {
+          name: "Pepper",
+          animal: "Bird",
+          breed: "Cockatiel"
+        })
     ]
   );
 };
