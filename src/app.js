d162676fb64e3928e5acb5fb92bff653c07d88d1
Enrico Massone <enrico.massone@deltatre.it>
2019-12-02 07:09:21 +0100

    created and reused a per component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+const Pet = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Luna"),
+      React.createElement("h2", {}, "Dog"),
+      React.createElement("h2", {}, "Havanese"),
+    ]
+  );
+};
+
 const App = () => {
   return React.createElement(
     "div", 
     {},
-    React.createElement("h1", {}, "Adopt me!"));
+    [
+      React.createElement("h1", {}, "Adopt me!"),
+      React.createElement(Pet),
+      React.createElement(Pet),
+      React.createElement(Pet)
+    ]
+  );
 };
 
 const root = document.getElementById("root");
