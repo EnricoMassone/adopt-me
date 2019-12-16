8e54eaf107c94f17a89373665bdca6c47e949101
Enrico Massone <enrico.massone@deltatre.it>
2019-12-16 07:28:17 +0100

    rewritten pet component from scratch

--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, media, location, id }) => {
+  let heroImage = "http://placecorgi.com/300/300";
+  if (media && media.length) {
+    heroImage = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={heroImage} alt={name} />
+      </div>
+
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
