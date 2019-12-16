5f10c6b0844a206a4bb7a697cbefe795f0792254
Enrico Massone <enrico.massone@deltatre.it>
2019-12-16 08:54:01 +0100

    added the router to the page

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./searchParams";
+import { Router, Link } from "@reach/router";
+import Details from "./details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt me!</h1>
-        <SearchParams />
+
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
