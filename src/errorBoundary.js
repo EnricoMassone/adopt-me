7367e6bb0822918076c17002e58bfb6c519403df
Enrico Massone <enrico.massone@deltatre.it>
2019-12-24 08:00:50 +0100

    created a component able to handle errors (error boundary)

---
+++ src/errorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log("An error has occurred and has been handled", error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    if (hasError) {
+      return (
+        <h1>
+          An error has occurred. <Link to="/">Click here</Link> to go back to
+          the home page or wait for five seconds.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
